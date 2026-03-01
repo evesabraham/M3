e2c08914639fa8561d002147cb076cfddc66f73a
evesabraham <esabraham@eths202.org>
2026-03-01 22:18:53 +0000

    git commit -m<type>:<cost calculator>

---
+++ costs.py
@@ -0,0 +1,86 @@
+import math
+costs = []
+bare_costs = []
+
+income = int(input("income: "))
+age = int(input("age: "))
+
+def food_cost(i):
+    temp = 30.5723*(i**0.5146)
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def utility_cost(i):
+    temp = 130.451*(i**0.3209)
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def clothing_cost(i):
+    temp = 2.02778*(10**(-17))*(i**4)-((6.43284*(10**(-12)))*(i**3))+(5.81473*(10**(-7))*(i**2))-0.00296238*i+857.78279
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def entertainment_cost(i):
+    temp = 1.3833*(10**(-7))*(i**2)+0.006175*i+1360.978
+    costs.append(temp)
+
+def transportation_cost(i):
+    temp = round((73718.7586)/(1+(math.pow((math.e),(-(0.0000101536*i-2.57732))))))
+    costs.append(temp)
+    
+def healthcare_cost(a):
+    temp = 898-30.2*a+0.692*(a**2)
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def childcare_cost(i):
+    temp = 0.104*i+4481
+    costs.append(temp)
+
+def education_cost(i):
+    temp = (151097.226)/(1+math.pow(math.e,(-(0.00000772441*i-1.60384))))
+    costs.append(temp)
+    
+
+def housing1(i):
+    temp = 9972+((59772)/(1+math.pow(math.e,((-0.000013)*(i-223759)))))
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def housing2(i):
+    temp = 9676*(math.pow(math.e,(0.0000067*i)))+792
+    costs.append(temp)
+    bare_costs.append(temp)
+
+def housing3(i):
+    temp = 887*(math.pow(math.e, (0.0000067*i)))+792
+    costs.append(temp)
+    bare_costs.append_temp
+    
+food_cost(income)
+utility_cost(income)
+clothing_cost(income)
+entertainment_cost(income)
+transportation_cost(income)
+healthcare_cost(age)
+childcare_cost(income)
+if age<=36 and age>=18:
+    education_cost(income)
+if income<106500 or age<=36 and age>=18:
+    housing1(income)
+elif age<=50 and age>=36:
+    housing2(income)
+elif age>=51:
+    housing3(income)
+
+total1=0
+total2=0
+
+for i in costs:
+    total1+=i
+
+for i in bare_costs:
+    total2+=i
+
+print(income-total1)
+print(income-total2)
